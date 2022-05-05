26cec63fe6361d3e5781f97ccb0908657fb22861
BangHyeonJun <davidbang@cj.net>
2022-05-05 19:10:12 +0900

    API연동

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TaskManager from "./components/TaskManager";
+import axios from "axios";
 
 function App() {
 	const [todoList, setTodoList] = useState([]);
 
-	const changeTaskToComplete = ({ id }) => {};
+	useEffect(() => {
+		updateTodoList();
+	}, []);
 
-	const changeTaskToUnComplete = ({ id }) => {};
+	const updateTodoList = async () => {
+		const { data } = await fetchTodoList();
+		setTodoList(data);
+	};
 
-	const appendTaskInToDoList = ({ title }) => {};
+	const fetchTodoList = async () => {
+		const { data } = await axios.get(`http://localhost:1337/api/todos`);
+		return data;
+	};
 
-	const deleteTaskInToDoList = ({ id }) => {};
+	const changeTaskToComplete = (id) => async () => {
+		await axios.put(`http://localhost:1337/api/todos/${id}`, {
+			data: {
+				complete_yn: true,
+			},
+		});
+		updateTodoList();
+	};
+
+	const changeTaskToUnComplete = (id) => async () => {
+		await axios.put(`http://localhost:1337/api/todos/${id}`, {
+			data: {
+				complete_yn: false,
+			},
+		});
+		updateTodoList();
+	};
+
+	const appendTaskInToDoList = async (title) => {
+		await axios.post(`http://localhost:1337/api/todos`, {
+			data: {
+				title: title,
+				complete_yn: false,
+			},
+		});
+		updateTodoList();
+	};
+
+	const deleteTaskInToDoList = (id) => async () => {
+		await axios.delete(`http://localhost:1337/api/todos/${id}`);
+		updateTodoList();
+	};
 
 	return (
 		<div className="App">
--- src/components/Taskform.js
+++ src/components/Taskform.js
@@ -5,26 +5,26 @@ const Taskform = ({ onAppend }) => {
 	const [inputEntered, updateInput] = useState("");
 
 	const enterTextlistener = (e) => {
-		updateInput(() => e.target.value);
+		if (e.key === "Enter") {
+			onAppend(inputEntered);
+			updateInput("");
+		} else {
+			updateInput(() => e.target.value);
+		}
 	};
 
 	return (
-		<form
-			onSubmit={() => {
-				onAppend({ title: inputEntered });
-			}}
-		>
-			<div className="taskform">
-				<input
-					className="ipclass"
-					type="text"
-					pattern=".*"
-					placeholder="할 일을 추가해주세요"
-					value={inputEntered}
-					onChange={enterTextlistener}
-				/>
-			</div>
-		</form>
+		<div className="taskform">
+			<input
+				className="ipclass"
+				type="text"
+				pattern=".*"
+				placeholder="할 일을 추가해주세요"
+				value={inputEntered}
+				onChange={enterTextlistener}
+				onKeyPress={enterTextlistener}
+			/>
+		</div>
 	);
 };
 
